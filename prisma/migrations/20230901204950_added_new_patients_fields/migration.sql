e7bdca9221485144f0c35be3e7757c47fe6c0d9f
Fellipe Lorram <fellipelorram@gmail.com>
2023-09-04 11:41:48 -0300

    Fix(Patients): Adding new appointment fields

---
+++ prisma/migrations/20230901204950_added_new_patients_fields/migration.sql
@@ -0,0 +1,33 @@
+/*
+  Warnings:
+
+  - Added the required column `appointment_day` to the `patients` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_patients" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "address" TEXT,
+    "age" INTEGER NOT NULL,
+    "email" TEXT,
+    "gender" TEXT,
+    "observation" TEXT,
+    "nationality" TEXT,
+    "birthDate" DATETIME,
+    "modality" TEXT NOT NULL,
+    "appointment_duration" INTEGER NOT NULL,
+    "appointment_day" TEXT NOT NULL,
+    "appointment_time" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "patients_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_patients" ("address", "age", "appointment_duration", "appointment_time", "birthDate", "createdAt", "email", "gender", "id", "modality", "name", "nationality", "observation", "updatedAt", "user_id") SELECT "address", "age", "appointment_duration", "appointment_time", "birthDate", "createdAt", "email", "gender", "id", "modality", "name", "nationality", "observation", "updatedAt", "user_id" FROM "patients";
+DROP TABLE "patients";
+ALTER TABLE "new_patients" RENAME TO "patients";
+CREATE UNIQUE INDEX "patients_email_key" ON "patients"("email");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
